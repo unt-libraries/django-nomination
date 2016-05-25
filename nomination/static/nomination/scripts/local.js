56180558d2671b921cc24aceea924eea16cdb61e
jason-ellis <jason.c.ellis@gmail.com>
2016-05-25 12:56:21 -0500

    Delete unused vendor JS files and add updated versions in separate vendor directory. Combine custom JS into one file for less requests.

--- nomination/static/nomination/scripts/local.js
+++ nomination/static/nomination/scripts/local.js
@@ -1,110 +1,107 @@
-// JavaScript Document
-
-$(document).ready(function() {
-
-
-
-	// Make Headings Toggle their next siblings (the field wrapper) visibility
-	$('h2.collapse-next').next().hide().end().click(function() {
-			$(this).toggleClass('closed').next().slideToggle();		
-	});
-
-	/*
-	Handle the click events for add/remove fields.
-	Take into account that some DOM elements will be newly inserted or created asyncronysily so take advantage of event bubbling.
-	Assigning the functionality to the parent container to bind newly generated "remove" button
-	Upon click of the button:
-		clone
-		change id and name attribute of divs, inputs, textareas, and selects by couting siblings and increasing value +1
-		remove the add button
-		append the remove button
-		
-		other handlers:
-		
-		handle check/uncheck all checkboxes
-		copy GEO coordinates from google map to form field
-		generate pre-filled inputs when users "Add" subject/keywords using the treeview plugin
-		
-	See: http://www.learningjquery.com/2008/03/working-with-events-part-1
-	*/
-	$('.field-wrapper').click(function(event) {
-					var $tgt = $(event.target);		
-					if ($tgt.is("a[href$='#select_all']")) {
-						     $tgt.attr('href', '#select_none').text('Select None');
-            			     $("." + $tgt.attr('rel') + " input[type='checkbox']").attr('checked', true);
-            			     return false;
-					}
-					if ($tgt.is("a[href$='#select_none']")) {
-  						     $tgt.attr('href', '#select_all').text('Select All');
-           				     $("." + $tgt.attr('rel') + " input[type='checkbox']").attr('checked', false);
-           				     return false;	
-					}
-					if ($tgt.is("a[href$='#add_subject_to_form']")) {
-							var $subjectFormField = $('<input style="width:250px" disabled="disabled" type="text" name="subject-value" id="subject-value" value="" />')
-														.appendTo('#subject .element-wrapper');
-
-							$subjectFormField.each(function(index) {
-									$(this).attr('id', 'subject-value-' +
-											$(this)
-											.siblings('input')
-											.size());
-									$(this).attr('name', 'subject-value-' +
-											$(this)
-											.siblings('input')
-											.size())
-								})
-							 	.val($tgt.prev().text())
-								.after('<a class="remove-from-form" href="#remove_subject_form_field">Remove</a>');
-							 
-           				     return false;	
-					}
-					if ($tgt.is("a[href$='#remove_subject_form_field']")) {
-  						     $tgt.prev().remove();
-   						     $tgt.remove();
-           				     return false;	
-					}
-				});
-
-        $('#preview-url').click(function() {
-                                       // Get href from textbox
-                                       window.open(document.getElementById('url-value').value, '_blank');
-                                });
-// browse List. http://plugins.jquery.com/project/treeview
-
-// End Document Ready Function
-});
+/*
+ * Functions for autofilling form data within a session
+ */
 
 function saveInformation() {
-      // Save user information from the form!
-      $.cookie('your-name-value', $("input[@id='your-name-value']").val(), { path: '/' });
-      $.cookie('email-value', $("input[@id='email-value']").val(), { path: '/' });
-      $.cookie('institution-value', $("input[@id='institution-value']").val(), { path: '/' });
-   }
-   
-function getCookie(name) {
-    var dc = document.cookie;
-    var prefix = name + "=";
-    var begin = dc.indexOf("; " + prefix);
-    if (begin == -1) {
-        begin = dc.indexOf(prefix);
-        if (begin != 0) return null;
-    } else {
-        begin += 2;
-    }
-    var end = document.cookie.indexOf(";", begin);
-    if (end == -1) {
-        end = dc.length;
-    }
-    return unescape(dc.substring(begin + prefix.length, end));
+    // Save user information from the form in session cookies
+    Cookies.set('your-name-value', $("input[id='your-name-value']").val());
+    Cookies.set('email-value', $("input[id='email-value']").val());
+    Cookies.set('institution-value', $("input[id='institution-value']").val());
+}
+
+function retrieveInformation() {
+    // Set form values to information retrieved from cookies
+    $("input[id='your-name-value']").val(Cookies.get('your-name-value'));
+    $("input[id='email-value']").val(Cookies.get('email-value'));
+    $("input[id='institution-value']").val(Cookies.get('institution-value'));
+}
+
+function focusAddForm() {
+    // Focus on URL field after placeholder text
+    var $urlValue = $('#url-value').get(0);
+    var urlValueLen = $urlValue.value.length;
+    $urlValue.selectionStart = urlValueLen;
+    $urlValue.selectionEnd = urlValueLen;
+    $urlValue.focus();
 }
 
 function initForm() {
-  $("#url-value").focus();
-  // Autofill user information if available
-  $("input[@id='your-name-value']").val(getCookie('your-name-value'));
-  $("input[@id='email-value']").val(getCookie('email-value'));
-  $("input[@id='institution-value']").val(getCookie('institution-value'));
+    // Autofill user information if available
+    retrieveInformation();
 
-  // Save comment information when form is submitted
-  $("form").submit(saveInformation);
+    // Save comment information when form is submitted
+    $("form").submit(saveInformation);
+}
+
+// Toggle for check/uncheck all
+$('input[data-check-all="true"]').on('click', function() {
+
+    // Select all checkbox siblings of button, except "other"
+    var $checkboxes = $(this).siblings('.checkbox')
+                             .find('input[type="checkbox"]')
+                             .not('[value="other_specify"]');
+
+    // Use data field to determine check state
+    if( $(this).data('check-state') === 'select' ) {
+        $checkboxes.each(function() {
+            $(this).prop('checked', true);
+        });
+        $(this).attr('value', 'Deselect All').data('check-state', 'deselect');
+    } else {
+        $checkboxes.each(function() {
+            $(this).prop('checked', false);
+        });
+        $(this).attr('value', 'Select All').data('check-state', 'select');
+    }
+});
+
+// Use json_data in template to repopulate a form after form errors
+function repopulateForm() {
+    for (j=0; j<json_data.length; j++) {
+        if (json_data[j][1].length == 1 && json_data[j][1][0] == "") {
+            continue;
+        }
+        else {
+            ft = form_types[json_data[j][0]];
+            if (ft != undefined) {
+                if (ft == 'select' || ft == 'selectsingle') {
+                    for (k=0; k<json_data[j][1].length; k++) {
+                        for (m=0; m<document.getElementById(json_data[j][0]).options.length; m++) {
+                            if (document.getElementById(json_data[j][0]).options[m].value != json_data[j][1][k]) {
+                                continue;
+                            }
+                            else {
+                                document.getElementById(json_data[j][0]).options[m].selected = true;
+                            }
+                        }
+                    }
+                }
+                else if (ft == 'text' || ft == 'textarea' || ft == 'date') {
+                    document.getElementById(json_data[j][0]).value = json_data[j][1][0];
+                }
+                else if (ft == 'checkbox' || ft == 'radio') {
+                    for (k=0; k<json_data[j][1].length; k++) {
+                        for (m=0; m<document.getElementsByName(json_data[j][0]).length; m++) {
+                            if (document.getElementsByName(json_data[j][0])[m].value != json_data[j][1][k]) {
+                                continue;
+                            }
+                            else {
+                                document.getElementsByName(json_data[j][0])[m].checked = true;
+                            }
+                        }
+                    }
+                }
+            }
+            else {
+                // undefined case: do stuff for default fields
+                if (json_data[j][0] == 'url_value') {
+                    document.getElementById('url-value').value = json_data[j][1][0];
+                }
+                // handle user specified text fields (other_specify types)
+                else if (json_data[j][0].match(/^.*_other$/)) {
+                    document.getElementById(json_data[j][0]).value = json_data[j][1][0];
+                }
+            }
+        }
+    }
 }
