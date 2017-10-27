02ac927e0dc3b4fd119f37e69d75e7f5ef1c9bf6
Gio Gottardi <somexpert@gmail.com>
2017-10-27 13:06:38 -0500

    Fix all flake8 errors and update CHANGELOG.md

--- nomination/admin.py
+++ nomination/admin.py
@@ -4,9 +4,11 @@ from nomination.models import (Project, Project_Metadata, Metadata_Values,
                                Valueset_Values, Nominator, URL, Metadata,
                                Value, ValueSet)
 
+
 class ProjectMetadataInline(admin.TabularInline):
     model = Project_Metadata
     extra = 3
+
 
 class ProjectAdmin(admin.ModelAdmin):
     """ Project class that determines how comment appears in admin """
@@ -14,20 +16,23 @@ class ProjectAdmin(admin.ModelAdmin):
     inlines = [ProjectMetadataInline]
     ordering = ('project_slug',)
 
+
 class NominatorAdmin(admin.ModelAdmin):
     """ Nominator class that determines how comment appears in admin """
     list_display = ('nominator_name', 'nominator_email', 'nominator_institution', 'nominations')
     list_display_links = ('nominator_name',)
     ordering = ('nominator_name',)
 
+
 class URLAdmin(admin.ModelAdmin):
     """ URL class that determines how comment appears in admin """
-    list_display = ('get_project', 'get_nominator', 'entity_display', 'attribute', 'value', 'date',)
+    list_display = ('get_project', 'get_nominator', 'entity_display', 'attribute', 'value', 'date')
     list_filter = ('url_project', 'url_nominator', 'attribute', 'date',)
-    search_fields  = ('entity', 'attribute', 'value',)
+    search_fields = ('entity', 'attribute', 'value',)
     list_display_links = ('attribute', 'value',)
     ordering = ('entity', 'attribute',)
     date_hierarchy = 'date'
+
 
 class ProjectAdminForm(forms.ModelForm):
     """ Project class to specify how form data is handled in admin """
@@ -43,24 +48,29 @@ class ProjectAdminForm(forms.ModelForm):
         else:
             raise forms.ValidationError("The project slug must be alphanumeric.")
 
+
 class MetadataValuesInline(admin.TabularInline):
     model = Metadata_Values
     extra = 10
 
+
 class MetadataAdmin(admin.ModelAdmin):
-#    list_display = ('name', 'project', 'form_type', 'description', 'metadata_order', 'required')
     list_display = ('name',)
+#    list_display = ('name', 'project', 'form_type', 'description', 'metadata_order', 'required')
     inlines = [MetadataValuesInline]
     search_fields = ['name']
 #    list_filter = ('project', 'name', 'form_type', 'required')
+
 
 class ValueAdmin(admin.ModelAdmin):
     list_display = ('value', 'key',)
     search_fields = ['value', 'key']
 
+
 class ValuesetValuesInline(admin.TabularInline):
     model = Valueset_Values
     extra = 5
+
 
 class ValueSetAdmin(admin.ModelAdmin):
     list_display = ('name',)
--- nomination/urls.py
+++ nomination/urls.py
@@ -1,29 +1,37 @@
-from django.conf.urls import *
-from nomination.views import *
-from django.contrib.syndication.views import Feed
+from django.conf.urls import patterns, url
+from nomination.views import (
+    project_listing, robot_ban, nomination_about, nomination_help, url_lookup, search_json,
+    browse_json, project_dump, url_score_report, url_nomination_report, url_date_report,
+    url_report, surt_report, nominator_report, nominator_url_report, field_report, value_report,
+    reports_view, url_listing, url_surt, url_add, project_about, project_urls
+)
 from nomination.feeds import url_feed, nomination_feed
 
-urlpatterns = patterns('',
+urlpatterns = patterns(
+    '',
     url(r"^$", project_listing, name='project_listing'),
     url(r"^robots.txt$", robot_ban, name='robot_ban'),
     url(r"^about/$", nomination_about, name='nomination_about'),
     url(r"^help/$", nomination_help, name='nomination_help'),
-    #url(r"^about/$", include('django.contrib.flatpages.urls'), name='nomination_about'),
-    #url(r"^help/$", include('django.contrib.flatpages.urls'), name='nomination_help'),
+    # url(r"^about/$", include('django.contrib.flatpages.urls'), name='nomination_about'),
+    # url(r"^help/$", include('django.contrib.flatpages.urls'), name='nomination_help'),
     url(r"^([^/]+)/lookup/$", url_lookup, name='url_lookup'),
     url(r"^([^/]+)/search.json$", search_json, name='search_json'),
     url(r"^([^/]+)/browse/([^/]+)/browse.json$", browse_json, name='browse_json'),
     url(r"^([^/]+)/reports/projectdump/$", project_dump, name='project_dump'),
     url(r"^([^/]+)/reports/urls/score/$", url_score_report, name='url_score_report'),
-    url(r"^([^/]+)/reports/urls/nomination/$", url_nomination_report, name='url_nomination_report'),
+    url(r"^([^/]+)/reports/urls/nomination/$", url_nomination_report,
+        name='url_nomination_report'),
     url(r"^([^/]+)/reports/urls/date/$", url_date_report, name='url_date_report'),
     url(r"^([^/]+)/reports/urls/$", url_report, name='url_report'),
     url(r"^([^/]+)/reports/surts/$", surt_report, name='surt_report'),
-    url(r"^([^/]+)/reports/metadata/(nominator|institution)/$", nominator_report, name='nominator_report'),
-    url(r"^([^/]+)/reports/metadata/(nominator|institution)/([^/]+)/$", nominator_url_report, name='nominator_url_report'),
+    url(r"^([^/]+)/reports/metadata/(nominator|institution)/$", nominator_report,
+        name='nominator_report'),
+    url(r"^([^/]+)/reports/metadata/(nominator|institution)/([^/]+)/$", nominator_url_report,
+        name='nominator_url_report'),
     url(r"^([^/]+)/reports/metadata/([^/]+)/$", field_report, name='field_report'),
-    #commented out line below for line under that in case user input value contains '/'
-    #url(r"([^/]+)/reports/metadata/([^/]+)/([^/]+)/$", value_report, name='value_report'),
+    # commented out line below for line under that in case user input value contains '/'
+    # url(r"([^/]+)/reports/metadata/([^/]+)/([^/]+)/$", value_report, name='value_report'),
     url(r"^([^/]+)/reports/metadata/([^/]+)/((?:.|\s)+)/$", value_report, name='value_report'),
     url(r"^([^/]+)/reports/$", reports_view, name='reports_view'),
     url(r"^(?P<slug>[^/]+)/url/(?P<url_entity>[\w\W]+)/$", url_listing, name='url_listing'),
