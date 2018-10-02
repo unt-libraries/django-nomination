1443b1b760100990464e3dfb4a8902587ff7ed8c
Gio Gottardi <somexpert@gmail.com>
2018-10-02 10:55:29 -0500

    Remove markup-deprecated and simplejson and update setup.py.

--- tests/settings.py
+++ tests/settings.py
@@ -21,7 +21,6 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.sites',
-    'markup_deprecated',
     'nomination',
     'widget_tweaks',
 )
